6afeb460b98727fbe5237e6558ab881549adf765
Nazia Ali <naziaaliburong@gmail.com>
2024-12-04 17:53:47 +0500

    vercel deployement

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero({heroHeading, displayHeroBtn}){
         displayHeroBtn : displayHeroBtn || "true",
     };
     return (
-    <div className="px-4 py-5 my-5 pt-0 text-center ">
+    <div className="px-4 py-5 my-5 pt-0 text-center resp ">
         <h1 className="display-5 fw-bold text-body-emphasis main-heading">{HeroData.heroHeading}</h1>
         <div className="col-lg-6 mx-auto">
             <p className="lead mb-4 main-para">A free template by <a href="/" className="main-anchor">Colorlib</a>. Download and share!</p>
